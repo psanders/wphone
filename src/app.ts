c35bc25c7ebfe2fb27a1b348fc76b662084cf531
Pedro Sanders <sanderspedro@gmail.com>
2020-09-19 11:25:07 -0400

    Updated to support registration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { SimpleUser, SimpleUserOptions } from "sip.js/lib/platform/web/index.js";
 import { getAudio, getButton, getButtons, getInput, getSpan } from "./utils.js";
 
-const server = "wss://edge.sip.onsip.com";
-const aor = "sip:echo@sipjs.onsip.com";
+const server = 'ws://localhost:5062';
+const aor = 'sip:1002@sip.local';
+const authorizationUsername = '1002';
+const authorizationPassword = '1234';
 
 const options: SimpleUserOptions = {
   aor,
@@ -13,13 +15,18 @@ const options: SimpleUserOptions = {
     remote: {
       video: (document.getElementById('remoteVideo') as HTMLVideoElement)
     }
+  },
+  userAgentOptions: {
+    authorizationPassword,
+    authorizationUsername,
   }
 };
 
 const simpleUser = new SimpleUser(server, options);
 simpleUser.connect()
-.then(r => {
-  simpleUser.call('sip:echo@sipjs.onsip.com')
+.then(async(r) => {
+  await simpleUser.register()
+  await simpleUser.call('sip:ast@sip.local')
 })
 .catch(e => console.error(e));
 

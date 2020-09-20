ca29de69bad921fb298c950c8d1a39053a70f13a
Pedro Sanders <sanderspedro@gmail.com>
2020-09-19 23:08:07 -0400

    Improved configuration and added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM alpine:3.10
+LABEL maintainer="Pedro Sanders <psanders@fonoster.com>"
+
+COPY . /wphone
+WORKDIR /wphone
+
+RUN apk add --update nodejs=10.19.0-r0 npm=10.19.0-r0 ; \
+  npm -g i http-server && npm i && npm run build ; \ 
+  rm -rf /var/cache/apk/* /tmp/* /var/tmp/*
+
+CMD ["/bin/sh", "-c", "http-server", "."]
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import { SimpleUser, SimpleUserOptions } from "sip.js/lib/platform/web/index.js";
 import { getAudio, getButton, getButtons, getInput, getSpan } from "./utils.js";
 
-const server = 'ws://localhost:5062';
-const aor = 'sip:1002@sip.local';
-const authorizationUsername = '1002';
-const authorizationPassword = '1234';
+const server = process.env.WPHONE_SERVER || 'ws://localhost:5062';
+const aor = process.env.WPHONE_AOR || 'sip:1001@sip.local';
+const dest = process.env.WPHONE_DEST || 'sip:ast@sip.local';
+const authorizationUsername = process.env.WPHONE_AUTH_USERNAME || '1001';
+const authorizationPassword = process.env.WPHONE_AUTH_PASSWORD || '1234';
 
 const options: SimpleUserOptions = {
   aor,
@@ -26,7 +27,7 @@ const simpleUser = new SimpleUser(server, options);
 simpleUser.connect()
 .then(async(r) => {
   await simpleUser.register()
-  await simpleUser.call('sip:ast@sip.local')
+  await simpleUser.call(dest)
 })
 .catch(e => console.error(e));
 
